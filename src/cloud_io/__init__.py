298b2aa504066c0b82fb4bf5db783586f0d96df7
dpkkmr20 <deepakkmr3110@gmail.com>
2024-10-13 16:05:32 +0530

    scrape.py is iupdated and errors in other files is also changed

--- src/cloud_io/__init__.py
+++ src/cloud_io/__init__.py
@@ -11,7 +11,7 @@ class MongoIO:      #this class is used to interact with mongodb database for st
 
     def __init__(self):
         if MongoIO.mongo_ins is None:
-            mongo_db_url = "mongodb+srv://dpkkr:admin_dpk@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            mongo_db_url = MONGODB_URL_KEY
             if mongo_db_url is None:
                 raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
             MongoIO.mongo_ins = mongo(client_url=mongo_db_url,
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,4 +1,4 @@
-MONGODB_URL_KEY: str = "mongodb+srv://dpkkr:admin_dpk@cluster0.nqjw6nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URL_KEY: str = "mongodb+srv://dpkkr:dpkkr@cluster0.nqjw6nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 MONGO_DATABASE_NAME: str = "myntra-reviews"
 
 
